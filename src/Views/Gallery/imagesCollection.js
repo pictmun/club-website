6898a3b1675d347e83ff4530d7a1290aa91b109f
anujapatil31 <anujadp4@gmail.com>
2021-11-30 19:54:34 +0530

    Gallery Added

--- src/Views/Gallery/imagesCollection.js
+++ src/Views/Gallery/imagesCollection.js
@@ -1,9 +1,21 @@
 import img1 from "./images/img1.JPG";
 import img2 from "./images/img2.JPG";
 import img3 from "./images/img3.JPG";
-import img4 from "./images/img4.JPG";
+import img4 from "./images/img4.png";
 import img5 from "./images/img5.JPG";
 import img6 from "./images/img6.JPG";
+import img7 from "./images/img7.png";
+import img8 from "./images/img8.JPG";
+import img9 from "./images/img9.JPG";
+import img10 from "./images/img10.JPG";
+import img11 from "./images/img11.JPG";
+import img12 from "./images/img12.JPG";
+import img13 from "./images/img13.JPG";
+import img14 from "./images/img14.JPG";
+import img15 from "./images/img15.JPG";
+import img16 from "./images/img16.JPG";
+import img17 from "./images/img17.JPG";
+import img18 from "./images/img18.JPG";
 
 export const imagesCollection = [
   {
@@ -29,5 +41,54 @@ export const imagesCollection = [
   {
     id: 6,
     image: img6
-  }
+  },
+  {
+    id: 7,
+    image: img7
+  },
+  {
+    id: 8,
+    image: img8
+  },
+  {
+    id: 9,
+    image: img9
+  },
+  {
+    id: 10,
+    image: img10
+  },
+  {
+    id: 11,
+    image: img11
+  },
+  {
+    id: 12,
+    image: img12
+  },
+  {
+    id: 13,
+    image: img13
+  },
+  {
+    id: 14,
+    image: img14
+  },
+  {
+    id: 15,
+    image: img15
+  },
+  {
+    id: 16,
+    image: img16
+  },
+  {
+    id: 17,
+    image: img17
+  },
+  {
+    id: 18,
+    image: img18
+  },
+  
 ];
